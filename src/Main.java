cc9e3671a11f860b56d9163f2097114201f0c8ec
bibinur99 <binur22@mail.ru>
2023-05-08 14:52:05 +0600

    first commit to HW12, to be checked

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book first = new Book("To kill a Mockingbird", "Harper Lee", 1960);
+        Book second  = new Book(  " ""Drei Kammeraden"", "Erich Maria Remark", 1936);
+        System.out.println("first book is " + first.getName() + ", "
+                + first.getAuthor()+ ", " + first.getYear());
+        System.out.println("second book is " + second.getName() + ", " + second.getAuthor()+
+                ", "+ second.getYear());
+
+        Author lee = new Author("Harper", "Lee");
+        System.out.println("lee.authorName = " + lee.getAuthorName() + " " +
+                lee.getAuthorSurname());
+        Author remark = new Author("Erich Maria", "Remark");
+        System.out.println("remark.authorName = " + " " + remark.getAuthorName()+
+                " " + remark.getAuthorSurname());
+        first.setYear(1961);
+        System.out.println("first book's change: " + first.getYear());
+
     }
 }
